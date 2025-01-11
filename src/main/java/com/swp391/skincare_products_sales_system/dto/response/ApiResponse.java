d0c85c28b4c3cb49818e1d9959dc34913f2e9924
dominhit215@gmail.com <dominhit215@gmail.com>
2025-01-11 14:01:16 +0700

    feat: add ApiResponse

--- src/main/java/com/swp391/skincare_products_sales_system/dto/response/ApiResponse.java
+++ src/main/java/com/swp391/skincare_products_sales_system/dto/response/ApiResponse.java
@@ -1,4 +1,17 @@
 package com.swp391.skincare_products_sales_system.dto.response;
 
-public class ApiResponse {
+import com.fasterxml.jackson.annotation.JsonInclude;
+import lombok.*;
+import lombok.experimental.FieldDefaults;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@JsonInclude(JsonInclude.Include.NON_NULL)
+@FieldDefaults(level = AccessLevel.PRIVATE)
+public class ApiResponse<T> {
+    int code;
+    String message;
+    T result;
 }
