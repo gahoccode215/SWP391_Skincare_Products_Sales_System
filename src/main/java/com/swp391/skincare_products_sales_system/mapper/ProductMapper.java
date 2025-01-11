70a70134feb88f0bc4f9b9132ab4cd82ab02bcb3
dominhit215@gmail.com <dominhit215@gmail.com>
2025-01-11 14:05:26 +0700

    feat: add toProduct and toProductResponse in ProductMapper

--- src/main/java/com/swp391/skincare_products_sales_system/mapper/ProductMapper.java
+++ src/main/java/com/swp391/skincare_products_sales_system/mapper/ProductMapper.java
@@ -1,8 +1,13 @@
 package com.swp391.skincare_products_sales_system.mapper;
 
+import com.swp391.skincare_products_sales_system.dto.request.ProductCreationRequest;
+import com.swp391.skincare_products_sales_system.dto.response.ProductResponse;
+import com.swp391.skincare_products_sales_system.pojo.Product;
+import com.swp391.skincare_products_sales_system.pojo.User;
 import org.mapstruct.Mapper;
 
 @Mapper(componentModel = "spring")
 public interface ProductMapper {
-    
+    Product toProduct(ProductCreationRequest request);
+    ProductResponse toProductResponse(User user);
 }
