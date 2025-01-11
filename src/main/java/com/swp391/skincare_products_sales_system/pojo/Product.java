9a2549fc6ac9dfab5de2114973fe514d4804b266
dominhit215@gmail.com <dominhit215@gmail.com>
2025-01-11 13:27:59 +0700

    feat: add ProductEntity

---
+++ src/main/java/com/swp391/skincare_products_sales_system/pojo/Product.java
@@ -0,0 +1,20 @@
+package com.swp391.skincare_products_sales_system.pojo;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+import lombok.*;
+import lombok.experimental.FieldDefaults;
+
+@Entity
+@Table()
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+@Builder
+@FieldDefaults(level = AccessLevel.PRIVATE)
+public class Product extends Base{
+    String title;
+    double price;
+    String description;
+}
