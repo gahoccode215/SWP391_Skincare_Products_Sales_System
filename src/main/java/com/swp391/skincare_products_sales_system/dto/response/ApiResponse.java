f8e7476f6e2dc448e39a4f32c533a3aa1a4e79d1
dominhit215@gmail.com <dominhit215@gmail.com>
2025-01-11 13:50:40 +0700

    feat: add ProductCreationRequest

---
+++ src/main/java/com/swp391/skincare_products_sales_system/dto/response/ApiResponse.java
@@ -0,0 +1,4 @@
+package com.swp391.skincare_products_sales_system.dto.response;
+
+public class ApiResponse {
+}
---
+++ src/main/java/com/swp391/skincare_products_sales_system/mapper/ProductMapper.java
@@ -0,0 +1,8 @@
+package com.swp391.skincare_products_sales_system.mapper;
+
+import org.mapstruct.Mapper;
+
+@Mapper(componentModel = "spring")
+public interface ProductMapper {
+    
+}
