f91863166a8c5ab1c26effdf4d04ab6119fb29cc
dominhit215@gmail.com <dominhit215@gmail.com>
2025-01-13 17:17:03 +0700

    feat: add category pojo

---
+++ src/main/java/com/swp391/skincare_products_sales_system/pojo/Category.java
@@ -0,0 +1,18 @@
+package com.swp391.skincare_products_sales_system.pojo;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+import lombok.*;
+import lombok.experimental.FieldDefaults;
+
+@Entity
+@Table(name = "categories")
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+@Builder
+@FieldDefaults(level = AccessLevel.PRIVATE)
+public class Category extends Base{
+    String title;
+}
