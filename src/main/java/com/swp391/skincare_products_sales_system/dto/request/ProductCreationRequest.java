57e1ba3a3cdb1dcd3903782061282bb0666e1d43
dominhit215@gmail.com <dominhit215@gmail.com>
2025-01-14 18:11:32 +0700

    feat: add User

--- src/main/java/com/swp391/skincare_products_sales_system/dto/request/ProductCreationRequest.java
+++ src/main/java/com/swp391/skincare_products_sales_system/dto/request/ProductCreationRequest.java
@@ -3,12 +3,14 @@ package com.swp391.skincare_products_sales_system.dto.request;
 import lombok.*;
 import lombok.experimental.FieldDefaults;
 
+import java.io.Serializable;
+
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
 @Builder
 @FieldDefaults(level = AccessLevel.PRIVATE)
-public class ProductCreationRequest {
+public class ProductCreationRequest implements Serializable {
     String title;
     double price;
 }
--- src/main/java/com/swp391/skincare_products_sales_system/pojo/User.java
+++ src/main/java/com/swp391/skincare_products_sales_system/pojo/User.java
@@ -1,4 +1,23 @@
 package com.swp391.skincare_products_sales_system.pojo;
 
-public class User {
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+import lombok.*;
+import lombok.experimental.FieldDefaults;
+
+@Entity
+@Table(name = "users")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@FieldDefaults(level = AccessLevel.PRIVATE)
+public class User extends Base{
+    String email;
+    String password;
+    String firstName;
+    String lastName;
+    String phone;
+    String address;
 }
