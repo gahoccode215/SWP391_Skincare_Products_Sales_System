f373408eb313b97b6db8b01cdb907bb5dbe140a4
dominhit215@gmail.com <dominhit215@gmail.com>
2025-01-14 21:30:20 +0700

    feat: add EnumPattern and EnumPatternValidate

--- src/main/java/com/swp391/skincare_products_sales_system/dto/request/UserCreationRequest.java
+++ src/main/java/com/swp391/skincare_products_sales_system/dto/request/UserCreationRequest.java
@@ -1,14 +1,28 @@
 package com.swp391.skincare_products_sales_system.dto.request;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
 import com.swp391.skincare_products_sales_system.util.UserStatus;
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotNull;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.experimental.FieldDefaults;
+import org.springframework.format.annotation.DateTimeFormat;
 
+import java.util.Date;
+
+@Getter
+@Setter
+@FieldDefaults(level = AccessLevel.PRIVATE)
 public class UserCreationRequest {
     @Email(message = "Email invalid format")
     String email;
     @NotNull(message = "Password must be not null")
     String password;
+    @DateTimeFormat(iso = DateTimeFormat.ISO.DATE)
+    @JsonFormat(pattern = "MM/dd/yyyy")
+    Date dateOfBirth;
     UserStatus userStatus;
     String firstName;
     String lastName;
--- src/main/java/com/swp391/skincare_products_sales_system/util/EnumPattern.java
+++ src/main/java/com/swp391/skincare_products_sales_system/util/EnumPattern.java
@@ -1,9 +1,24 @@
 package com.swp391.skincare_products_sales_system.util;
 
+import jakarta.validation.Constraint;
+import jakarta.validation.Payload;
+
 import java.lang.annotation.*;
 
+import static java.lang.annotation.ElementType.*;
+import static java.lang.annotation.RetentionPolicy.RUNTIME;
+
+/**
+ * Check enum by regular expression
+ */
 @Documented
-@Retention(RetentionPolicy.RUNTIME)
-@Target({ElementType.METHOD, ElementType.FIELD, ElementType.ANNOTATION_TYPE, ElementType.CONSTRUCTOR, ElementType.PARAMETER, ElementType.TYPE_USE})
+@Retention(RUNTIME)
+@Target({METHOD, FIELD, ANNOTATION_TYPE, CONSTRUCTOR, PARAMETER, TYPE_USE})
+@Constraint(validatedBy = EnumPatternValidator.class)
 public @interface EnumPattern {
+    String name();
+    String regexp();
+    String message() default "{name} must match {regexp}";
+    Class<?>[] groups() default {};
+    Class<? extends Payload>[] payload() default {};
 }
