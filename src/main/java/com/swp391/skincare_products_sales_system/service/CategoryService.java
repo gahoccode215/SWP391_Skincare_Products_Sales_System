0f8e07253f6ed4ca83c8ddeb14d44da5f2e1a2f5
dominhit215@gmail.com <dominhit215@gmail.com>
2025-01-14 11:29:49 +0700

    feat: merge category into dev

---
+++ src/main/java/com/swp391/skincare_products_sales_system/service/CategoryService.java
@@ -0,0 +1,4 @@
+package com.swp391.skincare_products_sales_system.service;
+
+public interface CategoryService {
+}
---
+++ src/main/java/com/swp391/skincare_products_sales_system/service/CategoryServiceImpl.java
@@ -0,0 +1,14 @@
+package com.swp391.skincare_products_sales_system.service;
+
+import lombok.AccessLevel;
+import lombok.RequiredArgsConstructor;
+import lombok.experimental.FieldDefaults;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+@FieldDefaults(level = AccessLevel.PRIVATE, makeFinal = true)
+public class CategoryServiceImpl implements CategoryService{
+    
+
+}
