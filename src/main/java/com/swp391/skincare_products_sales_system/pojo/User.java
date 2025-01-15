823df261884582b76696c22808a705eadc78917b
dominhit215@gmail.com <dominhit215@gmail.com>
2025-01-15 12:12:00 +0700

    feat: change UserEntity

--- src/main/java/com/swp391/skincare_products_sales_system/pojo/User.java
+++ src/main/java/com/swp391/skincare_products_sales_system/pojo/User.java
@@ -1,15 +1,19 @@
 package com.swp391.skincare_products_sales_system.pojo;
 
+import com.swp391.skincare_products_sales_system.util.Gender;
 import com.swp391.skincare_products_sales_system.util.UserStatus;
-import jakarta.persistence.Entity;
-import jakarta.persistence.Table;
+import jakarta.persistence.*;
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotNull;
 import lombok.*;
 import lombok.experimental.FieldDefaults;
+import org.hibernate.annotations.JdbcTypeCode;
+import org.hibernate.type.SqlTypes;
+
+import java.util.Date;
 
 @Entity
-@Table(name = "users")
+@Table(name = "tbl_user")
 @Getter
 @Setter
 @NoArgsConstructor
@@ -17,10 +21,29 @@ import lombok.experimental.FieldDefaults;
 @Builder
 @FieldDefaults(level = AccessLevel.PRIVATE)
 public class User extends Base{
+
+    @Column(name = "email", unique = true, nullable = false)
     String email;
+    @Column(name = "password", nullable = false)
     String password;
+    @Column(name = "firstName")
     String firstName;
+    @Column(name = "lastName")
     String lastName;
+    @Column(name = "phone")
     String phone;
-    String address;
+
+    @Column(name = "date_of_birth")
+            @Temporal(TemporalType.DATE)
+    Date dateOfBirth;
+
+    @Enumerated(EnumType.STRING)
+            @JdbcTypeCode(SqlTypes.NAMED_ENUM)
+            @Column(name = "gender")
+    Gender gender;
+
+    @Enumerated(EnumType.STRING)
+    @JdbcTypeCode(SqlTypes.NAMED_ENUM)
+    @Column(name = "status")
+    UserStatus userStatus;
 }
