62a2e452589a423dc5b3c40c4e8664c378e2b7bd
dominhit215@gmail.com <dominhit215@gmail.com>
2025-03-12 17:05:39 +0700

    feat: refactor Category management

--- src/main/java/com/swp391/skincare_products_sales_system/repository/CategoryRepository.java
+++ src/main/java/com/swp391/skincare_products_sales_system/repository/CategoryRepository.java
@@ -16,7 +16,6 @@ public interface CategoryRepository extends JpaRepository<Category, String> {
     Optional<Category> findByIdAndIsDeletedFalse(String id);
 
     @Query("SELECT c FROM Category c WHERE c.isDeleted = false " +
-            "AND c.status = com.swp391.skincare_products_sales_system.enums.Status.ACTIVE " +
             "AND c.slug = :slug")
     Optional<Category> findBySlugAndStatusAndIsDeletedFalse(@Param("slug") String slug);
 
@@ -24,15 +23,9 @@ public interface CategoryRepository extends JpaRepository<Category, String> {
 
     @Query("SELECT c FROM Category c WHERE c.isDeleted = false " +
             "AND (c.name LIKE %:keyword% OR :keyword IS NULL) "
-            + "AND (:status is null OR c.status = :status)"
     )
     Page<Category> findAllByFilters(
             @Param("keyword") String keyword,
-            @Param("status") Status status,
             Pageable pageable);
 
-
-    @Modifying
-    @Query("UPDATE Category c SET c.status = :status WHERE c.id = :id AND c.isDeleted = false")
-    void updateCategoryStatus(@Param("id") String id, @Param("status") Status status);
 }
